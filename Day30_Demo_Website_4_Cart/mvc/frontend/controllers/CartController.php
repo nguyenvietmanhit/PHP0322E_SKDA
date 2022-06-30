7075af5172cb1840075d94b8a9354e01e749c022
Manhnv <nguyenvietmanhit@gmail.com>
2022-06-30 19:48:08 +0700

    update

--- Day30_Demo_Website_4_Cart/mvc/frontend/controllers/CartController.php
+++ Day30_Demo_Website_4_Cart/mvc/frontend/controllers/CartController.php
@@ -1,8 +1,41 @@
 <?php
+//frontend/controllers/CartController.php
 require_once 'controllers/Controller.php';
 require_once 'models/Product.php';
 
 class CartController extends Controller
 {
-
+    public function add() {
+        $product_id = $_GET['product_id'];
+        // Lấy thông tin sp theo id
+        $product_model = new Product();
+        $product = $product_model->getById($product_id);
+//        echo '<pre>';
+//        print_r($product);
+//        echo '</pre>';
+        $cart_item = [
+            'name' => $product['title'],
+            'avatar' => $product['avatar'],
+            'price' => $product['price'],
+            'quantity' => 1 //Mặc định mỗi lần thêm 1 sp vào giỏ
+        ];
+        // - Logic xử lý thêm sp vào giỏ:
+        // + Nếu giỏ hàng chưa từng tồn tại, tạo giỏ hàng và thêm
+        //sp đầu tiên vào giỏ
+        // + Nếu giỏ hàng đã tồn tại:
+        // - Nếu sp thêm tồn tại trong giỏ, update quantity lên 1
+        // - NẾu sp thêm chưa tồn tại, thêm mới
+        if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'][$product_id] = $cart_item;
+        } else {
+            if (isset($_SESSION['cart'][$product_id])) {
+                $_SESSION['cart'][$product_id]['quantity']++;
+            } else {
+                $_SESSION['cart'][$product_id] = $cart_item;
+            }
+        }
+        echo '<pre>';
+        print_r($_SESSION['cart']);
+        echo '</pre>';
+    }
 }
--- Day30_Demo_Website_4_Cart/note.php
+++ Day30_Demo_Website_4_Cart/note.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * - Chức năng giỏ hàng:
+ * + Lưu giỏ hàng bằng cách thức nào? cookie, session,
+ * database ...
+ * -> sử dụng session
+ * + Kết hợp kỹ thuật Ajax khi Thêm sp vào giỏ hàng,
+ * tạo hiệu ứng tốt cho user
+ * + Cấu trúc của giỏ hàng:
+ */
+$_SESSION['cart'] = [
+    5 => [
+        'name' => 'IPhone X',
+        'avatar' => 'ip.png',
+        'price' => 1000,
+        'quantity' => 3
+    ],
+    2 => [
+        'name' => 'IPhone X2',
+        'avatar' => 'ip2.png',
+        'price' => 2000,
+        'quantity' => 2
+    ]
+];
